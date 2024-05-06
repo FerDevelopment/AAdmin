2c3c87bf3e06a9d481eae8da0737dd277cc6079b
fernando <fernando@fernando-Linux>
2024-05-06 14:19:10 +0200

    mk

--- SquAAre/src/main/Creator.java
+++ SquAAre/src/main/Creator.java
@@ -1,5 +1,15 @@
 package main;
 
-public class Creator {
+public class Creator extends Persona {
+	private String creatorCode = "";
+	private String dmca = "";
 
+	public Creator(String code, String dmca) {
+		this.creatorCode=code;
+		this.dmca=dmca;
+	}
+	
+	public String cosas() {
+		return creatorCode+dmca;
+	}
 }
